ae70d68a8160431c66571803c326b9dc0dfef392
eseoseodion <ehodion1@sheffield.ac.uk>
2024-05-02 19:41:10 +0100

    Created a database model for the restaurant menu

---
+++ restaurant_menu/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+MEAL_TYPE = (
+    ("starters", "Starters"),
+    ("salads", "Salads"),
+    ("main_dishes", "Main Dishes"),
+    ("desserts", "Desserts")
+)
+
+STATUS = {
+    (0, "Unavailable"),
+    (1, "Available")
+}
+
+
+# Inherits from the models class of the model library
+class Item(models.Model): 
+    meal = models.CharField(max_length=1000, unique=True)
+    description = models.CharField(max_length=2000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    meal_type = models.CharField(max_length=200, choices=MEAL_TYPE)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=STATUS, default=0)
+    date_created = models.DateTimeField(auto_now_add=True) # records when a dish is added to the menu
+    date_updated = models.DateTimeField(auto_now=True) # records when a dish is updated/edited
+
+
+    def __str__(self):
+        return self.meal
+    
+
