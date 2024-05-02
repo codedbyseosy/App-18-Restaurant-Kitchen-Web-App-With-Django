37ee45df5569b06d21174646c3c1b788d30a14c7
eseoseodion <ehodion1@sheffield.ac.uk>
2024-05-02 23:27:11 +0100

    Added python context to index.html file

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views import generic
 from .models import Item
@@ -7,6 +8,11 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    # This is a pre-defined method that we are over-writing
+    def get_context_data(self): 
+        context = {"meals":["Pizza", "Pasta"],
+                   "ingredients": ["things"]} # this is a dictionary
+        return context
 
 class MenuItemDetail(generic.DetailView):
     model = Item
